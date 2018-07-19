09a3548fde682d152c092f4da71db2787eb5712e
Unknown <nicolas.bertin0@gmail.com>
2018-07-19 10:40:44 +0200

    functionnal Mailer class

--- lib/app/townhalls_mailer.rb
+++ lib/app/townhalls_mailer.rb
@@ -1,3 +1,29 @@
-salut
+require 'gmail'
+require 'mail'
 
-gsg
+arr = [{"ville" => "Grenoble", "email" => "nicolas.bertin0@gmail.com", "departement" => "Isère"},
+{"ville" => "Meylan", "email" => "ludovic.bourgoin@gmail.com", "departement" => "Isère"}]
+
+
+class Mailer
+  def initialize(arr)
+    @arr = arr
+  end
+
+  def send_emails
+    gmail = Gmail.connect("bonjambonneau38", "thpthp38")
+
+    @arr.each do |town|
+
+      email = gmail.compose do
+        to town["email"]
+        subject "Je spam des gens "
+        body "Fiacre"
+      end
+      email.deliver! # or: gmail.deliver(email)
+    end
+    gmail.logout
+  end
+end
+
+emails = Mailer.new(arr).send_emails
